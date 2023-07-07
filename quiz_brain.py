85ab0de913ff33368367d1951514e4ed317a1cb7
Deniss Solovjovs <deniss11sol@gmail.com>
2023-07-07 23:21:52 +0300

    New features

--- quiz_brain.py
+++ quiz_brain.py
@@ -3,7 +3,7 @@ from question_model import Question
 
 class QuizBrain:
 
-    def __init__(self, q_list):
+    def __init__(self, q_list=[]):
         self.question_number = 0
         self.score = 0
         self.question_list = q_list
